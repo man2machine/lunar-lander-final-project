45c87f7953b7fe9c4efaeead95786ded7c72e4fd
Sathwik Karnik <skarnik2012@gmail.com>
2021-05-25 01:57:01 -0400

    example of training with ppo

---
+++ lander/run_ppo.py
@@ -0,0 +1,17 @@
+import gym
+from lunar_lander_old3 import LunarLander, LunarLanderContinuous
+from stable_baselines3 import PPO
+
+env = LunarLanderContinuous()
+model = PPO("MlpPolicy", env)
+model.learn(total_timesteps=10000)
+
+obs = env.reset()
+for i in range(1000):
+    action, _states = model.predict(obs, deterministic=True)
+    obs, reward, done, info = env.step(action)
+    env.render()
+    if done:
+      obs = env.reset()
+
+env.close()
